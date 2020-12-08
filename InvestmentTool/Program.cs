4450b791fefe750b6be38286bf7799e525e39041
peter <peter@LAPTOP-BL6787KK>
2020-12-08 21:23:48 +0100

    created a simple method to get the current price out of the txt file without using endless switch cases for every investment

--- InvestmentTool/Program.cs
+++ InvestmentTool/Program.cs
@@ -22,20 +22,23 @@ namespace VermoegenPrototyp
         {
             string action = Convert.ToString(Console.ReadLine());
             //initializing investments
-            Investment tesla = new Investment("Tesla");
-            Investment lufth = new Investment("Lufthansa");
             switch (action)
             {
+                //switch case am ende auch besser programmieren mit weniger code
                 case "check":
+                    Investment tesla = new Investment("Tesla");
+                    Investment lufth = new Investment("Lufthansa");
                     Console.WriteLine("\nYour returns:");
                     Console.WriteLine("\nINVESTMENT\tWEEKLY\tTOTAL");
                     //TESLA
                     Console.WriteLine(tesla.Name + "\t" + tesla.RenditeBerechnen("week") + "%\t" + tesla.RenditeBerechnen("abs") + "%");
                     //LUFTHANSA
                     Console.WriteLine(lufth.Name + "\t" + lufth.RenditeBerechnen("week") + "%\t" + lufth.RenditeBerechnen("abs") + "%");
+                    //whichaction noch einmal aufrufen mit einer if davor ob man noch was machen will
                     break;
                 case "input":
                     //user input into txt file for every investment + dont forget date!!
+                    //whichaction noch einmal aufrufen mit einer if davor ob man noch was machen will
                     break;
                 default:
                     Console.WriteLine("Type in valid command. (check/input)");
@@ -46,79 +49,58 @@ namespace VermoegenPrototyp
     }
     class Investment
     {
-        //fields
-        private decimal _CurrentPrice;
+        //contructor
+        public Investment(string _Name)
+        {
+            Name = _Name;
+            GetCurrent(Name);
+            GetBuyIn(Name);
+        }
 
-        //properties
-        public string Name { get; set; }
-        public Investment(string investmentName)
-        {
-            Name = investmentName;
-        }
-        public decimal CurrentPrice
-        {
-            get
-            {
-                int rows;
-                decimal currentP;
-                switch(Name)
-                {
-                    case "Tesla":
-                        string _tesla = @"data\tesla.txt";
-                        rows = File.ReadLines(_tesla).Count();
-                        currentP = Convert.ToDecimal(File.ReadLines(_tesla).Skip(rows - 1).Take(1).First());
-                        return currentP;
-                    case "Lufthansa":
-                        string _lufth = @"data\lufthansa.txt";
-                        rows = File.ReadLines(_lufth).Count();
-                        currentP = Convert.ToDecimal(File.ReadLines(_lufth).Skip(rows - 1).Take(1).First());
-                        return currentP;
-                    default:
-                        return 404;
-                }
-            }
-            set
-            {
-                _CurrentPrice = value;
-            }
-        }
-        public decimal Kaufwert { get; set; }
-        public decimal WertVor1Woche { get; set; }
+        //fields
+        public string Name;
+        private decimal CurrentPrice;
+        private decimal BuyInPrice;
+        private decimal PriceAWeekAgo;
 
         //methods
+        private void GetCurrent(string title)
+        {
+            string path = @"data\" + Name + ".txt";
+            int rows = File.ReadLines(path).Count();
+            CurrentPrice = Convert.ToDecimal(File.ReadLines(path).Skip(rows - 1).Take(1).First());
+        }
+        private void GetBuyIn(string title)
+        {
+
+        }
+
         public decimal RenditeBerechnen(string Rendite)
         {
             switch (Rendite)
             {
                 case "abs":
                     decimal absoluteRendite;
-                    if (AktuellerWert - Kaufwert < 0)
+                    if (CurrentPrice - BuyInPrice < 0)
                     {
-                        absoluteRendite = ((AktuellerWert - Kaufwert) / Kaufwert) * 100;
+                        absoluteRendite = ((CurrentPrice - BuyInPrice) / BuyInPrice) * 100;
                         return absoluteRendite;
                     }
                     else
                     {
-                        absoluteRendite = ((AktuellerWert - Kaufwert) / AktuellerWert) * 100;
+                        absoluteRendite = ((CurrentPrice - BuyInPrice) / CurrentPrice) * 100;
                         return absoluteRendite;
                     }
                 case "week":
-                    decimal woechendlicheRendite;
-                    decimal WertVor1Woche;
+                    decimal weeklyReturn;
                     switch (Name)
                     {
                         case "Tesla":
-                            string _tesla = @"data\tesla.txt";
-                            //herausfinden wie viele Zeilen die Datei hat
-                            int Zeilen = File.ReadLines(_tesla).Count();
-                            WertVor1Woche = Convert.ToDecimal(File.ReadLines(_tesla).Skip(Zeilen - 2).Take(1).First());
-                            AktuellerWert = Convert.ToDecimal(File.ReadLines(_tesla).Skip(Zeilen - 1).Take(1).First());
-                            woechendlicheRendite = ((AktuellerWert - WertVor1Woche) / WertVor1Woche) * 100;
-                            return woechendlicheRendite;
+                            weeklyReturn = ((CurrentPrice - PriceAWeekAgo) / PriceAWeekAgo) * 100;
+                            return weeklyReturn;
                         case "Lufthansa":
-                            WertVor1Woche = 30;
-                            woechendlicheRendite = ((AktuellerWert - WertVor1Woche) / WertVor1Woche) * 100;
-                            return woechendlicheRendite;
+                            weeklyReturn = ((CurrentPrice - PriceAWeekAgo) / PriceAWeekAgo) * 100;
+                            return weeklyReturn;
                         default:
                             return 404;
                     }
